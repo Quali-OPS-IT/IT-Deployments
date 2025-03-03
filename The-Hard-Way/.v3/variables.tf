e60374fd7432560ce451a595bda8896dd7e6005d
DorP_Quali <127408226+DorPoldian@users.noreply.github.com>
2025-03-03 13:51:21 +0200

    Add Terraform configurations for AWS resources including KMS, EC2, S3, IAM roles, and VPC

---
+++ The-Hard-Way/.v3/variables.tf
@@ -0,0 +1,46 @@
+# AWS Region
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "eu-west-1"
+}
+
+# AWS Key Pair for SSH Access
+variable "aws_key_name" {
+  description = "The AWS key pair name for accessing the EC2 instance"
+  type        = string
+}
+
+# VPC CIDR Block
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+# Subnet CIDR Block
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+# EC2 Instance Type
+variable "ec2_instance_type" {
+  description = "EC2 instance type for Terraform learning VM"
+  type        = string
+  default     = "t3.micro"
+}
+
+# Enable Cloud9 (Optional)
+variable "enable_cloud9" {
+  description = "Set to true to create a Cloud9 environment"
+  type        = bool
+  default     = false
+}
+
+# Bucket Name Prefix for Terraform State
+variable "s3_bucket_prefix" {
+  description = "Prefix for the S3 bucket name used for Terraform state"
+  type        = string
+  default     =
