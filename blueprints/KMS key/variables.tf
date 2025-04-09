deefff24fc4ed0f087d7b53bd911cefae9e4f503
DorPoldian <dor.p@quali.com>
2025-04-09 15:56:21 +0300

    kms-key fixes

--- blueprints/KMS key/variables.tf
+++ blueprints/KMS key/variables.tf
@@ -8,6 +8,11 @@ variable "description" {
     default = "KMS Key from TF"
 }
 
+variable "deletion_window_in_days" {
+    type = number
+    default = "10"
+}
+
 variable "enable_key_rotation" {
     type = bool
     default = true
