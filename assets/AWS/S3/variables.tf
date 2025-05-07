f7aa77708d7a0e0f4fad2c3c72a7b6c9c6d97deb
DorPoldian <dor.p@quali.com>
2025-05-07 14:23:44 +0300

    added validation to bucket_name var

--- assets/AWS/S3/variables.tf
+++ assets/AWS/S3/variables.tf
@@ -1,10 +1,27 @@
 variable "bucket_name" {
-  description = "The name of the S3 bucket (must be globally unique)"
+  description = "The name of the S3 bucket (must be globally unique, alphanumeric, and between 3 to 63 characters long)"
   type        = string
+
+  validation {
+    condition     = length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63
+    error_message = "The bucket name must be between 3 and 63 characters long."
+  }
+
+  validation {
+    condition     = can(regex("^[a-z0-9.-]+$", var.bucket_name))
+    error_message = "The bucket name must contain only lowercase letters, numbers, periods, and hyphens."
+  }
 }
+
 
 variable "region" {
   description = "The AWS region to create the S3 bucket in"
   type        = string
   default     = "eu-west-1"
+}
+
+variable "agent" {
+  description = "The Torque agent that will be used to create the S3 bucket"
+  type        = string
+  default     = "arn:aws:iam::123456789012:role/my-role"
 }
