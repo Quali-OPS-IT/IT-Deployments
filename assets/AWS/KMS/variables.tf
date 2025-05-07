3ad2a6f9066b3544203bf54ae322027a9fed7c1a
DorPoldian <dor.p@quali.com>
2025-05-07 14:46:36 +0300

    added validation to deletion window & added default agent (AWS-EKS)

--- assets/AWS/KMS/variables.tf
+++ assets/AWS/KMS/variables.tf
@@ -1,19 +1,29 @@
 variable "region" {
-    type = string
-    default = "eu-west-1"
-  
+  type    = string
+  default = "eu-west-1"
 }
+
 variable "description" {
-    type = string
-    default = "KMS Key from TF"
+  type    = string
+  default = "KMS Key from TF"
 }
 
 variable "deletion_window_in_days" {
-    type = number
-    default = "10"
+  type    = number
+  default = 10
+
+  validation {
+    condition     = var.deletion_window_in_days >= 7 && var.deletion_window_in_days <= 30
+    error_message = "The deletion window in days must be between 7 and 30 days."
+  }
 }
 
 variable "enable_key_rotation" {
-    type = bool
-    default = true
+  type    = bool
+  default = true
+}
+
+variable "agent" {
+  type    = string
+  default = "it-deployments-eks"
 }
